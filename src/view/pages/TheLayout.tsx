cc9f452fe61f033060c1d50622da3e0365b27229
zonaetmunna <zonaetmonna@gmail.com>
2022-04-05 12:24:40 +0600

    addd

---
+++ src/view/pages/TheLayout.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from '../components/common/Footer';
+import Header from '../components/common/Header';
+
+const TheLayout = () => {
+     return (
+          <div>
+               <Header></Header>
+               <Footer></Footer>
+          </div>
+     );
+};
+
+export default TheLayout;
---
+++ src/view/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+     return (
+          <div>
+               <h1>hu</h1>
+               <h1>this is home</h1>
+          </div>
+         
+     );
+};
+
+export default Home;
